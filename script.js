86e6c1e838c5f6689b60b2392e8a0339ff34add4
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 23:01:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 //your code here
+//your code here
+const bandnames = ['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Anthrax', 'The Who'];
 
-// Array of band names
-let bandNames = ['The Beatles', 'Metallica', 'Red Hot Chili Peppers', 'Aerosmith', 'The Rolling Stones'];
-
-// Function to remove articles from band names
-function removeArticles(name) {
-  // Regular expression to match articles 'a', 'an', 'the' at the beginning of the name
-  const regex = /^(a|an|the)\s/i;
-  
-  // Remove the article from the name and return the modified name
-  return name.replace(regex, '');
+function strip(bandname){
+  return bandname.replace(/^(a |the |an )/i, '').trim();
 }
 
-// Sort the band names without articles in lexicographic order
-const sortedBandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+const sortedbands = bandnames.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-// Get the ul element by its id
-const bandList = document.getElementById('band');
+const bandlist = document.querySelector('#bands');
 
-// Create li elements for each band name and append them to the ul element
-sortedBandNames.forEach((bandName) => {
+for (let i = 0; i < sortedbands.length; i++) {
   const li = document.createElement('li');
-  li.textContent = bandName;
-  bandList.appendChild(li);
-});
+  li.textContent = sortedbands[i];
+  bandlist.appendChild(li);
+}
